87d32b79c696c481bc7a7006a6759ec07fc99e83
zly01858966 <zly01858966@lazada.com>
2024-04-22 17:12:29 +0800

    chore: op code

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import Versions from './components/Versions.vue'
+import AppWindow from './components/app-window.vue'
 
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
+  <div class="app-wrapper">
+    <div class="left"></div>
+    <AppWindow link="https:\/\/3.cn/1Y1wH-A1" />
   </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
 </template>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/renderer/src/components/app-window.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  link: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="app-window-wrapper">
+    <iframe :src="link"></iframe>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app-window-wrapper {
+  width: 360px;
+  height: 720px;
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    resize: both;
+    border: none;
+  }
+}
+</style>
